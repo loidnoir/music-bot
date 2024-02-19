4abe9475bab8fb533e4eaedfc141124f724214b3
Samvel <loidnoir@gmail.com>
2024-02-19 21:54:17 +0400

    fix: runtime error

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import BaseClient from '@structures/BaseClient'
 
 async function start() {
 	const client = new BaseClient()
+	console.log(process.env.TOKEN)
 	await client.start()
 }
 
--- src/constants/ClientSecrets.ts
+++ src/constants/ClientSecrets.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config({ path: __dirname + '/../.env' })
+
 const ClientSecrets = {
-	token: process.env.DEV == 'true' ? process.env.TOKEN : process.env.DEV_TOKEN,
-	clientId: process.env.DEV == 'true' ? process.env.CLIENT_ID : process.env.DEV_CLIENT_ID,
+	token: process.env.TOKEN,
+	clientId: process.env.CLIENT_ID
 }
 
 export default ClientSecrets
