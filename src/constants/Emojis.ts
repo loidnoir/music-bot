fc2b4a4d01cf7c0ebf82d20ffc1c80ef4c8b4366
Samvel <loidnoir@gmail.com>
2024-02-20 01:06:09 +0400

    feat: emojis

---
+++ src/constants/Emojis.ts
@@ -0,0 +1,6 @@
+const Emojis = {
+	'warning': '<:Warning:1089216700790079640>',
+	'warning_img': 'https://cdn.discordapp.com/emojis/1089216700790079640.webp?size=96&quality=lossless'
+}
+
+export default Emojis
