747db17e2bf13a6fdce714a5d0ffadab42e0d751
Samvel <loidnoir@gmail.com>
2024-02-20 01:05:33 +0400

    feat: better help ux

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -1,6 +1,8 @@
+import { ClientColors } from '@constants/ClientPreferences'
 import BaseClient from '@structures/BaseClient'
 import BaseCommand, { CommandData, CommandSettings } from '@structures/BaseCommand'
-import { Message } from 'discord.js'
+import GuildModel from '@structures/GuildModel'
+import { EmbedBuilder, Message, roleMention } from 'discord.js'
 
 export default class Help extends BaseCommand {
 	public data: CommandData = {
@@ -13,7 +15,19 @@ export default class Help extends BaseCommand {
 		cooldown: 0
 	}
 
-	public run(client: BaseClient, msg: Message<true>): void {
-		msg.reply('## Ինչ ասեմ ախպեր\n- Եմբեդ համարյա չկա\n- Սարքածա սիրով\n- Արփիներին մուտք չկա\n- Բագ տնողը վատ մարդա, Ալեն')
+	public async run(client: BaseClient, msg: Message<true>): Promise<void> {
+		const guildModel = await GuildModel.cache(client, msg.guildId)
+		const prefix = guildModel.data.prefix
+
+		const content =
+			`- DJ \`${prefix}setdj [@role, disable]\` ${guildModel.data?.djRole ? roleMention(guildModel.data.djRole) : ''}\n` +
+			`- Prefix \`${prefix}setprefix [prefix]\``
+
+		const embed = new EmbedBuilder()
+			.setColor(ClientColors.secondary)
+			.setAuthor({ name: 'Հաճախ չտրվող հարցեր', iconURL: client.user?.displayAvatarURL() })
+			.setDescription(content)
+
+		await msg.channel.send({ embeds: [embed] })
 	}
 }
