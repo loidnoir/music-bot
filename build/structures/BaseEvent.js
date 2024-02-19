fdfa617b5fd06607afc63d2ebab24d751bb3263f
Samvel <loidnoir@gmail.com>
2024-02-19 21:55:22 +0400

    fix: build deletion

--- build/structures/BaseEvent.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseEvent {
-}
-exports.default = BaseEvent;
-//# sourceMappingURL=BaseEvent.js.map
--- build/structures/ChatCommand.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const BaseCommand_1 = tslib_1.__importDefault(require("@structures/BaseCommand"));
-class ChatCommand extends BaseCommand_1.default {
-}
-exports.default = ChatCommand;
-//# sourceMappingURL=ChatCommand.js.map
